7bd4841ad89c92701264f57be34adf2821ee764c
Mateusz Owczarek <mateuszowczarek@onet.eu>
2024-02-05 00:33:47 +0100

    network and cache

--- sw.js
+++ sw.js
@@ -13,40 +13,49 @@ const staticAssets = [
     'src/pdfs/regexp.md',
     'src/icons/512.png',
 ];
-  self.addEventListener('install', async (e) => {
-	const cache = await caches.open(cacheName);
-	await cache.addAll(staticAssets);
-	return self.skipWaiting();
+
+self.addEventListener('install', async (e) => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-	self.clients.claim();
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', async (e) => {
-	const req = e.request;
-	const url = new URL(req.url);
-	if (url.origin === location.origin) {
-		e.respondWith(cacheFirst(req));
-	} else {
-		e.respondWith(networkAndCache(req));
-	}
+    const req = e.request;
+    const url = new URL(req.url);
+    
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkAndCache(req));
+    }
 });
 
 async function cacheFirst(req) {
-	const cache = await caches.open(cacheName);
-	const cached = await cache.match(req);
-	return cached || fetch(req);
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+
+    return cached || fetch(req);
 }
 
 async function networkAndCache(req) {
-	const cache = await caches.open(cacheName);
-	try {
-		const fresh = await fetch(req);
-		await cache.put(req, fresh.clone());
-		return fresh;
-	} catch (e) {
-		const cached = await cache.match(req);
-		return cached;
-	}
+    const cache = await caches.open(cacheName);
+
+    try {
+        const fresh = await fetch(req);
+
+        if (staticAssets.includes(req.url)) {
+            // Clone the response before caching it
+            await cache.put(req, fresh.clone());
+        }
+
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
 }
